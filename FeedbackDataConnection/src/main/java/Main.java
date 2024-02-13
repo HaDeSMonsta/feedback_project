d9cd631dfea4e19c8c0771f3223ed0c0acbe6aa5
HaDeSMonsta <leon.poellinger+git@gmail.com>
2024-02-13 17:35:52 +0100

    Refactor code to use constants for filename and port
    Constants for the filename and port have been declared and used, thus improving code readability and maintainability. Moreover, the format of the date-time written into the file has been slightly altered for better understanding and it now registers it in Coordinated Universal Time (UTC).

--- FeedbackDataConnection/src/main/java/Main.java
+++ FeedbackDataConnection/src/main/java/Main.java
@@ -2,15 +2,20 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
+	private static final String FILE_NAME = "/feedback/feedback.txt";
+	private static final int PORT = 8080;
+
 	public static void main(String[] args) {
 		System.out.println("Starting Server");
 
-		try (ServerSocket server = new ServerSocket(8080)) {
+		try (ServerSocket server = new ServerSocket(PORT)) {
 
 			// noinspection InfiniteLoopStatement
 			while(true) {
@@ -26,14 +31,14 @@ public class Main {
 	private static synchronized void logic(Socket sock) {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 		     BufferedWriter writer = new BufferedWriter(
-					 new FileWriter("/feedback/feedback.txt", true))) {
+					 new FileWriter(FILE_NAME, true))) {
 
 			writer.write("-".repeat(50));
 			writer.newLine();
 
-			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-			LocalDateTime now = LocalDateTime.now();
-			writer.write(dtf.format(now));
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("[yyyy-MM-dd - HH:mm:ss]");
+			LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+			writer.write(dtf.format(now) + "z");
 
 			writer.newLine();
 
