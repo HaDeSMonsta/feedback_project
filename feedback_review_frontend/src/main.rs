d0205115e9110d66be451ae6507d06578d8eba2e
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-04-16 15:28:10 +0200

    Add notFound

--- feedback_review_frontend/src/main.rs
+++ feedback_review_frontend/src/main.rs
@@ -6,12 +6,17 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[derive(Debug, Deserialize)]
 struct FeedbackResponse {
-    feedbacks: Option<Vec<String>>,
+    feedbacks: Option<String>,
 }
 
 #[derive(Debug, Serialize)]
 struct FeedbackRequest {
     date: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct FeedbackDates {
+    dates: Option<Vec<String>>,
 }
 
 #[derive(Properties, PartialEq)]
@@ -27,6 +32,9 @@ enum Route {
     Version,
     #[at("/:date")]
     Date { date: String },
+    #[not_found]
+    #[at("/404")]
+    NotFound,
 }
 
 fn switch(routes: Route) -> Html {
@@ -34,6 +42,7 @@ fn switch(routes: Route) -> Html {
         Route::Home => html! { <Home/> },
         Route::Date { date } => html! { <Date date={date}/>},
         Route::Version => html! { <Version/>},
+        Route::NotFound => html! { <NotFound/> },
     }
 }
 
@@ -50,7 +59,7 @@ fn app() -> Html {
 fn home() -> Html {
     html! {
         <>
-            <p>{ "Moin" }</p>
+            <p>{ "Home" }</p>
             <Footer/>
         </>
     }
@@ -58,9 +67,6 @@ fn home() -> Html {
 
 #[function_component(Date)]
 fn date(props: &DateProps) -> Html {
-
-    let feedbacks = use_state(|| Vec::<String>::new());
-
     html! {
         <>
             <p>{ props.date.clone() }</p>
@@ -93,6 +99,19 @@ fn footer() -> Html {
         <footer class={classes!("mt-8", "text-sm", "text-center", "text-gray-500", "dark:text-gray-400")}>
             { "Made with ❤ using Yew and Axum" }
         </footer>
+    }
+}
+
+#[function_component(NotFound)]
+fn not_found() -> Html {
+    let location = use_location().unwrap();
+
+    html! {
+        <>
+            <h1 class={classes!("text-3xl", "font-bold", "mb-4")}>{ "404" }</h1>
+            <p>{ format!("Path {:?} not found", location.path()) }</p>
+            <Footer/>
+        </>
     }
 }
 
